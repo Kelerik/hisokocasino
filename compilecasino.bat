15664dce91bef44a6e21fe94e6834303d4ee4e2a
Kelerik <vincent.ng39@gmail.com>
2021-10-11 14:48:16 -0400

    attempt at proper git practices

--- compilecasino.bat
+++ compilecasino.bat
@@ -1,4 +1,4 @@
-pyinstaller "C:\Dropbox\Actual Documents\PycharmProjects\hisokocasino\hisokocasino.py" ^
+pyinstaller "C:\Dropbox\Actual Documents\PycharmProjects\hisokocasino\index.py" ^
  --onefile ^
  --icon="C:\Dropbox\Actual Documents\PycharmProjects\hisokocasino\hisoCate.ico" ^
  --distpath "C:\Dropbox\Actual Documents\PycharmProjects\hisokocasino"
