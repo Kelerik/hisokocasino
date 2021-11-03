2a268fe9319a2fd57a79a48c3dfca497f5c0ab88
Kelerik <vincent.ng39@gmail.com>
2021-11-03 13:44:31 -0400

    file rename

--- compilecasino.bat
+++ compilecasino.bat
@@ -1,4 +1,4 @@
-pyinstaller "C:\Dropbox\Actual Documents\PycharmProjects\hisokocasino\index.py" ^
+pyinstaller "C:\Dropbox\Actual Documents\PycharmProjects\hisokocasino\main.py" ^
  --onefile ^
  --icon="C:\Dropbox\Actual Documents\PycharmProjects\hisokocasino\hisoCate.ico" ^
  --distpath "C:\Dropbox\Actual Documents\PycharmProjects\hisokocasino"
